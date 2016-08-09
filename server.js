951a72d253165c2434ea784cbcba432110f94641
Leopoldo Brines <leobrines@gmail.com>
2016-08-09 17:40:21 -0400

    Use dynamic ports to work heroku

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ var
 	server = require('http').Server(app);
 	// socket = require('socket.io')(server);
 
+var port = process.env.PORT || 5000;
+
 app.use('/', express.static('./'));
 
 app.get('/', function(req, res){
 	res.sendFile('./index.html');	
 });
 
-server.listen(8080, function (){
-	console.log('Server running on port 8080');
+server.listen(port, function (){
+	console.log('Server running on port ' + port );
 });
 
